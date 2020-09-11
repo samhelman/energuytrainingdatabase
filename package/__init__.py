6589e2924f17a37bd1837ceca3fc05e555e4be72
Sam Helman <07shelman@gmail.com>
2020-09-11 12:35:54 -0400

    Added heroku database to init file.

--- package/__init__.py
+++ package/__init__.py
@@ -8,7 +8,7 @@ from flask_restful import Api
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '59ad26e5cb630aab0c78a64b86d145483d2e387c47538ab46d3f64a88e06b62c'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///training_dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lpclwmwqhyggeh:ba1120790c77bd62cff3deaab8fa4b404a5e71be678146e53d6c5e1d2861072e@ec2-54-158-222-248.compute-1.amazonaws.com:5432/ddoap02q09ivuk'
 app.config['TEMPLATE_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
